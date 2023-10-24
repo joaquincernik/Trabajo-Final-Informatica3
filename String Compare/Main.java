52d18cd0987548f03c3f1db1aeaef3a381a7ae60
Bruno <115671955+BrunoGaribaldi@users.noreply.github.com>
2023-10-24 17:43:13 -0300

    Add files via upload

---
+++ String Compare/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    
+    
+    public static void main(String[] args) {
+        String nombre1 = "bruNo";
+        String nombre2 = "chaco";
+        int i = nombre1.trim().toLowerCase().compareTo(nombre2.trim().toLowerCase());
+        System.out.println(i);
+    }
+    
+}
