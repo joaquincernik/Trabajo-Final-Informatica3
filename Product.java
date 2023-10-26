b38488b7ad6d09af530196335051782aea29528a
Bruno <115671955+BrunoGaribaldi@users.noreply.github.com>
2023-10-26 08:26:16 -0300

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,12 @@
+public class Product {
+    String name; 
+    float price; 
+
+    public Product(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+    
+    
+ 
+}
